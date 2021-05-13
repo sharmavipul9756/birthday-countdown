4a7d482cd57b11dbb5745b5f1f615c01f7a15d23
Vipul Sharma <sharmavipul9756@gmail.com>
2021-05-13 11:18:32 +0530

    gungun

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const countdown = () => {
-    const countDate = new Date('December 10, 2021 00:00:00').getTime();
+    const countDate = new Date('Feburary 19, 2022 00:00:00').getTime();
     const now = new Date().getTime();
     const gap = countDate - now;
     const second = 1000;
